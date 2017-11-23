87299ead658eca870a89ae735f047ccb03e00165
Stefan Zink <zinks@zinksrv.zink.sulz.de>
2017-11-23 19:10:36 +0100

    add profiled scripts

---
+++ profiled/sz_syspath.sh
@@ -0,0 +1,9 @@
+#echo "/links/bin/$(hostname -s)"
+if [ -d /links/bin/$(hostname -s) ]; 
+then
+  if ! [[ $PATH =~ "/links/bin/$(hostname -s)" ]]
+  then
+    #echo "export PATH=${PATH}:/links/bin/$(hostname -s)"
+    export PATH=${PATH}:/links/bin/$(hostname -s)
+  fi
+fi  
