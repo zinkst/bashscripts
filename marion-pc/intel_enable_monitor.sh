1af41dcedc20ea950404da7816ef1809f2cd04e7
Marion Zink <marion@marion-pc.fritz.box>
2017-11-28 18:38:58 +0100

    add xrandr script for marion-pc

---
+++ marion-pc/intel_enable_monitor.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+setDisplayVariables()
+{
+  PANEL_ID="LVDS-1"
+  MONITOR_ID="DP-1"
+}
+  
+getConnectedMonitors()
+{
+  MONITOR_CONNECTED=`xrandr -q | grep ${MONITOR_ID} | awk '{print $2}'`
+  echo "MONITOR_CONNECTED="${MONITOR_CONNECTED}
+}
+
+
+option1()
+{
+  echo ${opt1}
+  CMD="xrandr --output ${MONITOR_ID} --primary --mode 3840x2160 --output ${PANEL_ID} --off "
+   echo ${CMD}
+   eval ${CMD}
+}
+
+
+option2()
+{  
+  echo ${opt2}
+  CMD="xrandr --output ${MONITOR_ID} --off --output ${PANEL_ID} --mode 1600x900 --primary"
+  echo ${CMD}
+  eval ${CMD}
+}
+
+option3()
+{  
+  echo ${opt3}
+  CMD="xrandr --output ${MONITOR_ID} --mode 3840x2160 --primary --output ${PANEL_ID} --mode 1600x900 --right-of ${MONITOR_ID}"
+  echo ${CMD}
+  eval ${CMD}
+}
+
+option4()
+{  
+  echo ${opt2}
+  CMD="xrandr --output ${MONITOR_ID} --mode 1600x900 --primary --output ${PANEL_ID} --same-as ${MONITOR_ID}"
+  echo ${CMD}
+  eval ${CMD}
+}
+
+automaticSettings()
+{
+    echo ${default}
+    if [[ ${MONITOR_CONNECTED} == "connected" ]]
+    then
+        option1
+    else 
+        option3
+    fi    
+}
+
+# main starts here
+echo "script to switch on and off attached monitors"
+echo "usage:"
+default="determines all connected monitors and enables them"
+opt1="enables Monitor only"
+opt2="enables LCD only"
+opt3="enables Panel secondary Monitor primary" 
+opt4="enabling Screen mirror"
+echo "${0}   -> ${default}"
+echo "${0} 1 -> ${opt1}"
+echo "${0} 2 -> ${opt2}"
+echo "${0} 3 -> ${opt3}"
+echo "${0} 4 -> ${opt4}"
+
+setDisplayVariables
+getConnectedMonitors
+case ${1} in
+    1 )
+        option1 ;;
+    2 )
+        option2 ;;
+    3 )
+        option3 ;;
+    4 )
+        option4 ;;
+    5 )
+        option5 ;;
+    6 )
+        option6 ;;
+    * )
+        automaticSettings ;;
+    
+esac
