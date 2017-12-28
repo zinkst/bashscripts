617baac3ab9432adfbf20d20a953a0ce60d7bad6
Stefan Zink <zinks@zinksrv.zink.sulz.de>
2017-12-28 12:18:53 +0100

    update pdfsam

--- pdfsam.sh
+++ pdfsam.sh
@@ -6,7 +6,8 @@ java_env open
 #JAVA_PROGRAM_DIR="/etc/alternatives/java_sdk_1.5.0/bin/"	
 #JAVA_PROGRAM_DIR="/usr/lib/jvm/jre-1.6.0-ibm/bin/"	
 PROGRAM_DIR="/home/share/pdfsam"	# use full path to Azureus bin dir
-STARTJAR_NAME="pdfsam-2.2.1.jar"
+cd ${PROGRAM_DIR}
+STARTJAR_NAME=$(ls pdfsam*.jar | cut -d '/' -f 2)
 #######################################
 
 echo "starting ${JAVA_PROGRAM_DIR}java -jar ${PROGRAM_DIR}/${STARTJAR_NAME} "
