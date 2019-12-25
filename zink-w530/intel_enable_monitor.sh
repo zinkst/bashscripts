7a95934cbf1960648b42b604d1e3b07b03fc00eb
Stefan Zink <zinks@zink-w530.fritz.box>
2019-12-25 22:27:33 +0100

    add scripts for zink-w530

---
+++ zink-w530/intel_enable_monitor.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+setDisplayVariables()
+{
+  PANEL_ID="LVDS-1"
+  PANEL_RESOLUTION="1600x900"
+  MONITOR_ID="DP-1"
+  MONITOR_RESOLUTION="2560x1440"
+  #MONITOR_RESOLUTION="3840x2160"
+  #MONITOR_ID="HDMI-1"
+  #MONITOR_RESOLUTION="1920x1080"
+}
+  
+getConnectedMonitors()
+{
+  MONITOR_CONNECTED=`xrandr -q | grep ${MONITOR_ID} | awk '{print $2}'`
+  echo "MONITOR_CONNECTED="${MONITOR_CONNECTED}
+}
+
+
+option1()
+{
+  echo ${opt1}
+  CMD="xrandr --output ${MONITOR_ID} --primary --mode ${MONITOR_RESOLUTION} --output ${PANEL_ID} --off "
+   echo ${CMD}
+   eval ${CMD}
+}
+
+
+option2()
+{  
+  echo ${opt2}
+  CMD="xrandr --output ${MONITOR_ID} --off --output ${PANEL_ID} --mode ${PANEL_RESOLUTION} --primary"
+  echo ${CMD}
+  eval ${CMD}
+}
+
+option3()
+{  
+  echo ${opt3}
+  CMD="xrandr --output ${MONITOR_ID} --mode ${MONITOR_RESOLUTION} --primary --output ${PANEL_ID} --mode ${PANEL_RESOLUTION} --right-of ${MONITOR_ID}"
+  echo ${CMD}
+  eval ${CMD}
+}
+
+option4()
+{  
+  echo ${opt2}
+  CMD="xrandr --output ${MONITOR_ID} --mode ${PANEL_RESOLUTION} --primary --output ${PANEL_ID} --same-as ${MONITOR_ID}"
+  echo ${CMD}
+  eval ${CMD}
+}
+
+automaticSettings()
+{
+    echo ${default}
+    if [[ ${MONITOR_CONNECTED} == "connected" ]]
+    then
+        option1
+    else 
+        option3
+    fi    
+}
+
+# main starts here
+echo "script to switch on and off attached monitors"
+echo "usage:"
+default="determines all connected monitors and enables them"
+opt1="enables Monitor only"
+opt2="enables LCD only"
+opt3="enables Panel secondary Monitor primary" 
+opt4="enabling Screen mirror"
+echo "${0}   -> ${default}"
+echo "${0} 1 -> ${opt1}"
+echo "${0} 2 -> ${opt2}"
+echo "${0} 3 -> ${opt3}"
+echo "${0} 4 -> ${opt4}"
+
+setDisplayVariables
+getConnectedMonitors
+case ${1} in
+    1 )
+        option1 ;;
+    2 )
+        option2 ;;
+    3 )
+        option3 ;;
+    4 )
+        option4 ;;
+    5 )
+        option5 ;;
+    6 )
+        option6 ;;
+    * )
+        automaticSettings ;;
+    
+esac
--- zink-w530/zink-w530_rsync_zinksrv.sh
+++ zink-w530/zink-w530_rsync_zinksrv.sh
@@ -3,31 +3,31 @@
 TGT_ROOT="/remote/zinksrv/nfs4/"
 REMOTEMOUNTPOINT=${TGT_ROOT}
 NFS_SERVER_URI="zinksrv:/" 
+CORRECTHOST="zink-w530"
 #SRC_ROOT="/remote/marion/cifs/"
 # with CIFS there are permissions problems also when running as root
 SRC_ROOT="/links/"
-LOG_ROOT="${SRC_ROOT}/marion-pc/rsync/"
-RSYNC_PARAMS="-av --one-file-system --exclude-from ${SRC_ROOT}marion-pc/rsync/exclude.txt"
+LOG_ROOT="${SRC_ROOT}/rsync/"
+RSYNC_PARAMS="-av --one-file-system --exclude-from ${SRC_ROOT}/rsync/exclude.txt"
 LOGFILENAME=$(basename "${0}" .sh)
 LASTRUN_FILENAME="${LOGFILENAME}.lastrun"
 MINS_SINCE_LASTRUN=-1500
 USE_SSH=false
 CHECK_LASTRUN=false
-CORRECTHOST="marion-pc"
 TRY_MOUNT_TGT="true"
 index="1 2 3 4"
-
-Directories[1]="marion-pc/persdata/Marion/MarionsPhotos"
-TargetDir[1]="data/marion-pc/persdata/Marion/MarionsPhotos"
+#index="4"
+Directories[1]="${CORRECTHOST}/persdata/Henry-local"
+TargetDir[1]="data/${CORRECTHOST}/persdata/Henry-local"
 MountTestFile[1]=${TGT_ROOT}"data/doNotDelete"
-Directories[2]="marion-pc/homes"
-TargetDir[2]="data/marion-pc/homes"
+Directories[2]="${CORRECTHOST}/homes"
+TargetDir[2]="data/${CORRECTHOST}/homes"
 MountTestFile[2]=${TGT_ROOT}"data/doNotDelete"
-Directories[3]="marion-pc/persdata/Marion/marion-marion-pc"
-TargetDir[3]="data/marion-pc/persdata/Marion/marion-marion-pc"
+Directories[3]="${CORRECTHOST}/persdata/Henry-local"
+TargetDir[3]="data/${CORRECTHOST}/persdata/Henry-local"
 MountTestFile[3]=${TGT_ROOT}"data/doNotDelete"
-Directories[4]="marion-pc/sysbkp"
-TargetDir[4]="data/marion-pc/sysbkp"
+Directories[4]="${CORRECTHOST}/sysbkp"
+TargetDir[4]="data/${CORRECTHOST}/sysbkp"
 MountTestFile[4]=${TGT_ROOT}"data/doNotDelete"
 
 . /root/bin/bkp_functions.sh
