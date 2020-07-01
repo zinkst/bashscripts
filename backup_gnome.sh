636803096f0eeac2f4b0245ef2b04f3cb2a6e9ca
Stefan Zink <Stefan.G.Zink+gh@gmail.com>
2020-07-01 21:25:45 +0200

    Add parameters to backup_gnome.sh

--- backup_gnome.sh
+++ backup_gnome.sh
@@ -1,15 +1,17 @@
 #!/bin/bash	
-BKP_DIR=${HOME}/Stefan-local/BackupsAndSettings/GnomeBackup
-
+BKP_DIR=${HOME}/local/BackupsAndSettings/GnomeBackup
 
 function backup {
   #cp -prv .local/share/gnome-shell/extensions ${BKP_DIR)/ 
+  if [ ! -d ${BKP_DIR} ];  then
+	  mkdir -p ${BKP_DIR}
+  fi	
   dconf dump /org/gnome/ > ${BKP_DIR}/org_gnome.dconf
   dconf dump /org/gnome/shell/extensions/ > ${BKP_DIR}/org_gnome_shell_extensions.dconf
 }
 
 function restore {
-	#dconf reset -f /org/gnome/
+  #dconf reset -f /org/gnome/
 	dconf load/org/gnome/ < ${BKP_DIR}/org_gnome.dconf
 	dconf load/org/gnome/shell/extensions/ < ${BKP_DIR}/org_gnome_shell_extensions.dconf
 	setup_gnome
@@ -25,6 +27,23 @@ function setup_gnome {
 	gsettings set org.gnome.desktop.interface show-battery-percentage true
 }
 
-#main
-backup
-#restore
+# main
+while getopts "sbc" OPTNAME
+do
+  case "${OPTNAME}" in
+    "s")
+      echo "Option setup_gnome ${OPTNAME} is specified"
+      setup_gnome
+      ;;
+    "b")
+      echo "Option backup ${OPTNAME} is specified"
+      backup
+      ;;
+    "c")
+      echo "Option restore ${OPTNAME} is specified"
+      restore
+      ;;
+  
+  esac
+  #echo "OPTIND is now $OPTIND"
+done
