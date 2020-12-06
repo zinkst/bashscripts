c07ae9bab6f79d0ca9b5fecc7ccc0b75c968e5c1
Stefan Zink <Stefan.G.Zink+gh@gmail.com>
2020-12-06 13:04:38 +0100

    add Autounlock for keepass

--- startKeepassxc.sh
+++ startKeepassxc.sh
@@ -1,2 +1,12 @@
-#/bin/sh
-env QT_QPA_PLATFORM=xcb keepassxc $1 &
+#!/bin/bash
+# enter secret into database
+#secret-tool store --label='Keepass' database Keepass.kdbx
+secret-tool lookup database Keepass.kdbx > /dev/null
+if [ $? -eq 0 ]; then
+  echo "lookup successful"
+  secret-tool lookup database Keepass.kdbx | env QT_QPA_PLATFORM=xcb keepassxc --pw-stdin ~/Dokumente/privat/access_store/stefans &
+else 
+  echo "lookup unsuccessful"
+  env QT_QPA_PLATFORM=xcb keepassxc $1 &
+fi
+# secret-tool lookup database Keepass.kdbx | keepassxc-cli show -s -q -a Password ~/Dokumente/privat/access_store/stefans alpenvereinaktiv.com 
