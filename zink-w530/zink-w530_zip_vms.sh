78aba548203f7041d0151cf13c228ea96fab5129
Stefan Zink <zinks@zink-w530.fritz.box>
2021-04-16 22:51:24 +0200

    Updates for zink-w530

--- zink-w530/zink-w530_zip_vms.sh
+++ zink-w530/zink-w530_zip_vms.sh
@@ -4,7 +4,7 @@ source /links/bin/bkp_functions.sh
 DATESTRING=$(date +'%Y%m%d')
 src_dir="/"
 bkp_dir="/links/sysbkp"
-CORRECTHOST="marion-pc"
+CORRECTHOST="zink-w530"
 checkCorrectHost
 usePigz=true
 usePigz=false
@@ -12,8 +12,8 @@ usePigz=false
 #src[1]=${src_dir}links/vms/windows8-virt/windows8-virt.vdi
 #tgt[1]=${bkp_dir}/${CORRECTHOST}_${DATESTRING}_windows8-virt.vdi
 
-src[0]=${src_dir}local/ntfs_c/vhds/marion-pc_win10pro.vhd
-tgt[0]=${bkp_dir}/${CORRECTHOST}_${DATESTRING}_marion-pc_win10pro.vhd
+src[0]=${src_dir}local/ntfs_c/vhds/win10.vhd
+tgt[0]=${bkp_dir}/${CORRECTHOST}_${DATESTRING}_$(hostname -s)_win10.vhd
 
 execute_command=(false false)
 index=(0)
