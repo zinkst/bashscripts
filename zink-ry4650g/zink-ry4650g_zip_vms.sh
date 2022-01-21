6c5f3512cf2e135dd3fb116afe92d1bede1b033a
Stefan Zink <Stefan.G.Zink+gh@gmail.com>
2022-01-21 08:35:57 +0100

    Add win11.vhd to backup script for zink-ry4650G

--- zink-ry4650g/zink-ry4650g_zip_vms.sh
+++ zink-ry4650g/zink-ry4650g_zip_vms.sh
@@ -6,11 +6,13 @@ usePigz=false
 
 src[0]=${src_dir}/win10.vhd
 tgt[0]=${bkp_dir}/${DATESTRING}_$(hostname -s)_win10.vhd
+src[1]=${src_dir}/win11.vhd
+tgt[1]=${bkp_dir}/${DATESTRING}_$(hostname -s)_win11.vhd
 
 
 
 execute_command=(false false false false false)
-index=(0)
+index=(0 1)
 
 compressCommand () 
 {
