dbc9998d6572f05a79867f6134f456bb07a340ba
Stefan Zink <Stefan.G.Zink+gh@gmail.com>
2022-09-22 09:24:12 +0200

    Fix zinksrv/stopServerServices.sh

--- zinksrv/stopServerServices.sh
+++ zinksrv/stopServerServices.sh
@@ -1,32 +1,10 @@
 #!/bin/bash
 
-SERVER_SERVICES=(php-fpm nginx mariadb mosquitto.service home-assistant deCONZ grafana-server influxdb)
+SERVER_SERVICES=(home-assist php-fpm nginx mariadb mosquitto.service deCONZ grafana-server influxdb)
 
-function serviceStatus () {
-  for srv in "${SERVER_SERVICES[@]}"; 
-  do 
-    echo "############ status of $srv ###############################";
-    cmd="systemctl status ${srv} --no-pager"
-    echo $cmd
-    eval $cmd
-    echo ""
-  done
-  podman ps
-}
-
-function serviceStop () {
-  for srv in "${SERVER_SERVICES[@]}"; 
-  do 
-    echo "############ status of $srv ###############################";
-    cmd="systemctl stop ${srv}"
-    echo $cmd
-    eval $cmd
-    echo ""
-  done
-}
 
 function serviceStart () {
-  START_ORDER=(2 5 6 7 3 4 1 0)
+  START_ORDER=(3 4 5 6 7 2 1 0)
   for idx in "${START_ORDER[@]}"; 
   do 
     echo "############ starting of ${SERVER_SERVICES[$idx]} ###############################";
@@ -37,6 +15,19 @@ function serviceStart () {
   done  
 }
 
+function serviceOperation () {
+  OPERATION=${1}
+  for srv in "${SERVER_SERVICES[@]}"; 
+  do 
+    echo "############ ${OPERATION} of $srv ###############################";
+    cmd="systemctl ${OPERATION} ${srv} ${2}"
+    echo $cmd
+    eval $cmd
+    echo ""
+  done
+}
+
+
 # main
 
 if [ -z "$1" ]; then
@@ -45,16 +36,22 @@ if [ -z "$1" ]; then
     echo "${0} -k "
     echo "To start services run"
     echo "${0} -s "
+    echo "To enbale services run"
+    echo "${0} -e "
+    echo "To disable services run"
+    echo "${0} -d "
     exit -1
 fi
 
-while getopts "sk" Option
+while getopts "sked" Option
 do
   case $Option in
-    k ) serviceStop;;
     s ) serviceStart;;
+    k ) serviceOperation stop;;
+    d ) serviceOperation disable;;
+    e ) serviceOperation enable;;
   esac
 done
 
-
-serviceStatus
+serviceOperation status --no-pager 
+podman ps
