40f2039952f0917405b340233d04529a163bfd11
Stefan Zink <Stefan.G.Zink+gh@gmail.com>
2023-09-19 22:36:16 +0200

    Add zink-pc4 scripts

---
+++ zink-pc4/zink-pc4_rsync_zinksrv.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# variables
+SRC_ROOT="/"
+SSH_HOST="zinksrv"
+SSH_TGT_ROOT="root@${SSH_HOST}:/local/data/zink-ry4650g/"
+TGT_ROOT="/remote/zinksrv/nfs4/"
+LOG_ROOT="/links/sysbkp/rsync/"
+RSYNC_PARAMS="-av -A --one-file-system --exclude-from /links/sysbkp/rsync_exclude.txt"
+CORRECTHOST=$(hostname -s)
+LOGFILENAME=$(basename "${0}" .sh)
+LASTRUN_FILENAME="${LOGFILENAME}.lastrun"
+MINS_SINCE_LASTRUN=-1500
+USE_SSH=false
+CHECK_LASTRUN=false
+REMOTEMOUNTPOINT=${TGT_ROOT}
+TRY_MOUNT_TGT="true"
+index="1"
+
+Directories[1]="local/data/zink-pc4"
+TargetDir[1]="data/zink-pc4/data/zink-pc4"
+MountTestFile[1]=${TGT_ROOT}"data/doNotDelete"
+# Directories[2]="local/data_hdd/zink-ry4650g"
+# TargetDir[2]="data/zink-ry4650g/data_hdd/zink-ry4650g"
+# MountTestFile[2]=${TGT_ROOT}"data/doNotDelete"
+
+
+. /links/bin/bkp_functions.sh
+
+# main routine
+setLogfileName ${LOGFILENAME}
+checkCorrectHost
+rsyncBkpParamCheck $@
+if [ ${CHECK_LASTRUN} == true ]
+then
+	checkLastRun
+fi
+if [ ${USE_SSH} == true ]
+then
+    if ping -c 1 ${SSH_HOST} # &> /dev/null
+	then
+		echo "exit code of ping ${SSH_HOST} = $?; doing rsync"
+		doRsyncWithTgtDir
+	else
+		echo "exit code of ping ${SSH_HOST} = $?; exiting and not doing rsync"
+	fi
+else	
+	doRsyncWithTgtDirAndMountTestFile
+fi	
+updateLastRunFile
+umount ${REMOTEMOUNTPOINT}
