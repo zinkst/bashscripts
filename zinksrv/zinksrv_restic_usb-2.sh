28ad234a1196553b4dfa91d9f4b61b0ebfbf57d7
Stefan Zink <Stefan.G.Zink+gh@gmail.com>
2023-09-19 13:00:50 +0200

    zinsrv restic_backup

--- zinksrv/zinksrv_restic_usb-2.sh
+++ zinksrv/zinksrv_restic_usb-2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # variables
 export SRC_ROOT="/"
-export TGT_ROOT="/run/media/BKP_ZINK_USB2-part1"
+export TGT_ROOT="/run/media/BKP_ZINK_USB_2-part1"
 export RESTIC_PATH="zinksrv_restic"
 export RESTIC_REPOSITORY="${TGT_ROOT}/${RESTIC_PATH}"
 export RESTIC_PASSWORD_FILE=/links/sysbkp/restic_pwd_file
@@ -9,15 +9,15 @@ export LOG_ROOT="/links/zinksrv/sysbkp/restic_logs/${RESTIC_PATH}"
 #RSYNC_PARAMS="-av -A --one-file-system --exclude-from /links/data/zinksrv/rsync_exclude.txt"
 LOGFILENAME=$(basename "${0}" .sh)
 CORRECTHOST="zinksrv"
-index="1 2 3 4 5 6 7"
-index="1 2 3 4"
+index="1 2 3 4 5 6 7 8 9"
+index="1 3"
 #MINS_SINCE_LASTRUN=-1500
 #USE_SSH=false
 #CHECK_LASTRUN=false
 
 
-Directories[1]="local/data/zinksrv/Photos/"
-Directories[2]="local/data/zinksrv/FamilienVideos/"
+Directories[1]="local/data/zinksrv/Photos"
+Directories[2]="local/data/zinksrv/FamilienVideos"
 Directories[3]="local/data/zinksrv/Musik"
 Directories[4]="local/data/zinksrv/persdata"
 Directories[5]="local/data/kinder2"
@@ -42,19 +42,21 @@ function initializeBackupStore {
 
 doResticWithTgtDirAndMountTestFile () 
 {
-  for ind in $index
-  do
-    if [ ! mountpoint -q "${TargetDir[ind]}" ]; then
-			echo "${TargetDir[ind]} not mounted skipping" | tee -a ${LOG_ROOT}${LOGFILENAME}
-		else
-      cmd="restic backup –exclude-caches --verbose \"${SRC_ROOT}${Directories[ind]}/"
-			echo "$cmd"
-			#eval "$cmd" | tee -a ${LOG_ROOT}${LOGFILENAME}
-		fi  
+  if mountpoint -q "${TGT_ROOT}"; then
+  	for ind in $index
+	do
+		cmd="restic backup –exclude-caches --verbose=2 \"${SRC_ROOT}${Directories[ind]}/\""
+		echo "$cmd"
+		eval "$cmd" | tee -a ${LOG_ROOT}${LOGFILENAME}
+    done		
+  else
+    echo "${TGT_ROOT} not mounted skipping" | tee -a ${LOG_ROOT}${LOGFILENAME}
+  fi
 }
 
 
 mkdir -p "${LOG_ROOT}"
 setLogfileName ${LOGFILENAME}
 checkCorrectHost
+echo LogFileName: ${LOG_ROOT}${LOGFILENAME}
 doResticWithTgtDirAndMountTestFile
