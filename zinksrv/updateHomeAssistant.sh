9a5f088c118986a4ba0fb35517ae5555e94d90bb
Stefan Zink <Stefan.G.Zink+gh@gmail.com>
2023-09-29 14:02:19 +0200

    add scripts for containerservices

---
+++ zinksrv/updateHomeAssistant.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+function main() {
+  update-homeassistant
+}
+
+function create-pod() {
+  podman run -d --name="home-assistant" --mount type=bind,source="/local/data/$(hostname -s)/srv/home-assist/home-assistant-config",target=/config -v /etc/localtime:/etc/localtime:ro --net=host homeassistant/home-assistant:stable
+}
+
+function pull-image() {
+  podman pull homeassistant/home-assistant:stable
+}  
+
+function update-homeassistant() {
+  pull-image
+  systemctl stop home-assist.service
+  podman stop home-assistant 
+  podman rm home-assistant
+  create-pod
+  systemctl start home-assist.service
+}
+
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+main $@
+
