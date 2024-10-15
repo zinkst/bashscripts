56027f27bd7893956b46142a53e7ab04b89ac69a
Stefan Zink <Stefan.G.Zink+gh@gmail.com>
2024-10-15 06:52:31 +0200

    Add restore function to backupNextcloudQuadlet.sh script

--- backupNextcloudQuadlet.sh
+++ backupNextcloudQuadlet.sh
@@ -21,6 +21,38 @@ function backupNextcloud () {
    run-cmd "${CMD}"
 }	
 
+function restoreNextcloud () {
+   CMD="systemctl stop nextcloud-pod.service"
+   run-cmd "${CMD}"
+   echo "restoring backup of nextcoud html "
+   CMD="mv ${NEXTCLOUD_ROOT_DIR}/html ${NEXTCLOUD_ROOT_DIR}/html_$(date +'%y%m%d_%H%M%S')"
+   run-cmd "${CMD}"
+   CMD="mkdir -p ${NEXTCLOUD_ROOT_DIR}/html"
+   run-cmd "${CMD}"
+   CMD="tar -xzf  ${BACKUP_DIR}/nextcloud_html.tgz --directory ${NEXTCLOUD_ROOT_DIR}/html"
+   run-cmd "${CMD}"
+   # restore of DB seems to be not necessary restoreing the db volume is sufficient
+   #  echo "creating mariadbdump backup of nextcloud db "
+   #  CMD="podman exec -it nextcloud-db mariadb-dump -u ${MARIADB_USER} -p${MARIADB_USER_PASSWORD} ${MARIADB_DATABASE_NAME} | gzip -9 > ${BACKUP_DIR}/mariadb_dump.sql.gz"
+   #  run-cmd "${CMD}"
+   echo "restoring backup of nextcloud db data volume"
+   CMD="mv ${NEXTCLOUD_ROOT_DIR}/db ${NEXTCLOUD_ROOT_DIR}/db_$(date +'%y%m%d_%H%M%S')"
+   run-cmd "${CMD}"
+   CMD="mkdir -p ${NEXTCLOUD_ROOT_DIR}/db"
+   run-cmd "${CMD}"
+   CMD="tar -xzf  ${BACKUP_DIR}/nextcloud_db.tgz --directory ${NEXTCLOUD_ROOT_DIR}/db"
+   run-cmd "${CMD}"
+   echo "Starting nextcloud pods from backup"
+   CMD="systemctl start nextcloud-pod.service" 
+   run-cmd "${CMD}"
+   CMD="echo Waiting 60 seconds && sleep 60" 
+   run-cmd "${CMD}"
+   # need to disable maintenance mode since nextcloud was in mainteance mode when backup was created
+   echo "Disabling maintenance mode" 
+   CMD="podman exec -it -u www-data nextcloud-app php occ maintenance:mode --off" 
+   run-cmd "${CMD}"
+}	
+
 function setEnvVars() {
   NEXTCLOUD_ROOT_DIR=$(yq -r '.NEXTCLOUD.ROOT_DIR' "${CONFIG_YAML}")
   MARIADB_DATABASE_NAME=$(yq -r '.MARIADB.DATABASE_NAME' "${CONFIG_YAML}")
