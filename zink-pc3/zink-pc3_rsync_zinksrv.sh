11ea9dd48bcc8877f0c22d31128ffa1fb7e05925
Stefan Zink <Stefan.G.Zink+gh@gmail.com>
2024-10-17 21:33:31 +0200

    fixes for zink-pc3

--- zink-pc3/zink-pc3_rsync_zinksrv.sh
+++ zink-pc3/zink-pc3_rsync_zinksrv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 CORRECTHOST="zink-pc3"
+export TGT_ROOT=${TGT_ROOT:-"/remote/zinksrv/nfs4/"}
+ 
 index="1"
 
 # SSH_HOST="zinksrv"
@@ -9,9 +11,9 @@ index="1"
 
 Directories[1]="local/data/${CORRECTHOST}"
 TargetDir[1]="data/${CORRECTHOST}/data"
-MountTestFile[1]=${TGT_ROOT}"data/doNotDelete"
+MountTestFile[1]="${TGT_ROOT}data/doNotDelete"
 
 . /links/bin/lib/bkp_functions.sh
 
 # main routine
-processRsyncBackup() $@
+processRsyncBackup $@
