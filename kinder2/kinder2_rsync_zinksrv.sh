d5815d9b8e0fa3db24f1dd1efdbff591c5b6ec6c
Stefan Zink <Stefan.G.Zink+gh@gmail.com>
2024-10-18 09:07:24 +0200

    Update kinder2 rsync script

--- kinder2/kinder2_rsync_zinksrv.sh
+++ kinder2/kinder2_rsync_zinksrv.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
-# variables
-SRC_ROOT="/"
-SSH_HOST="zinksrv"
-SSH_TGT_ROOT="root@${SSH_HOST}:/local/data/kinder2/"
-TGT_ROOT="/remote/zinksrv/nfs4/"
-LOG_ROOT="/links/rsync_folder/"
+
 CORRECTHOST="kinder2"
-RSYNC_PARAMS="-av -A --one-file-system --exclude-from /links/bin/${CORRECTHOST}/rsync_exclude.txt"
-LOGFILENAME=$(basename "${0}" .sh)
-LASTRUN_FILENAME="${LOGFILENAME}.lastrun"
-MINS_SINCE_LASTRUN=-1500
-USE_SSH=false
-CHECK_LASTRUN=false
-REMOTEMOUNTPOINT=${TGT_ROOT}
-TRY_MOUNT_TGT="true"
+TGT_ROOT="/remote/zinksrv/nfs4/"
 index="1 2"
 
 Directories[1]="local/data/kinder2"
@@ -27,24 +15,4 @@ MountTestFile[2]=${TGT_ROOT}"data/doNotDelete"
 . /links/bin/lib/bkp_functions.sh
 
 # main routine
-setLogfileName ${LOGFILENAME}
-checkCorrectHost
-rsyncBkpParamCheck $@
-if [ ${CHECK_LASTRUN} == true ]
-then
-	checkLastRun
-fi
-if [ ${USE_SSH} == true ]
-then
-    if ping -c 1 ${SSH_HOST} # &> /dev/null
-	then
-		echo "exit code of ping ${SSH_HOST} = $?; doing rsync"
-		doRsyncWithTgtDir
-	else
-		echo "exit code of ping ${SSH_HOST} = $?; exiting and not doing rsync"
-	fi
-else	
-	doRsyncWithTgtDirAndMountTestFile
-fi	
-updateLastRunFile
-umount ${REMOTEMOUNTPOINT}
+processRsyncBackup $@
