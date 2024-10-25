4b25cedee74f60f1b82e1bcfc6bc32d5a193097f
Stefan Zink <Stefan.G.Zink+gh@gmail.com>
2024-10-25 11:40:10 +0200

    Add grafana quadlet and quadletFunctions.sh

---
+++ quadlet/grafanaQuadlet.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+set -euo pipefail
+
+source /links/bin/lib/quadletFunctions.sh
+
+function CreateQuadlet() {
+  mkdir -p ${GRAFANA_DATA_DIR}
+  # chmod 777 -R ${GRAFANA_DATA_DIR}
+  # chown -R 65534:65534 ${GRAFANA_DATA_DIR}
+  # chcon -t container_file_t ${GRAFANA_DATA_DIR}
+  mkdir -p ${GRAFANA_ETC_DIR}
+  if [ ! -f ${GRAFANA_ETC_DIR}/grafana.ini ]; then
+    DO_CONFIG="true"
+    wget -O ${GRAFANA_ETC_DIR}/grafana.ini https://github.com/grafana/grafana/raw/refs/heads/main/conf/sample.ini
+  fi
+  cat <<EOF > ${QUADLET_DIR}/${SERVICE_NAME}.container 
+[Unit]
+Description=${SERVICE_NAME}
+Wants=network-online.target
+After=network-online.target
+
+[Container]
+Label=app=${SERVICE_NAME}
+AutoUpdate=${PODMAN_AUTO_UPDATE_STRATEGY}
+ContainerName=${SERVICE_NAME}
+Image=${CONTAINER_IMAGE}
+Network=${NETWORK_NAME}
+PublishPort=${GRAFANA_HTTP_PORT}:3000
+Volume=${GRAFANA_DATA_DIR}:/var/lib/grafana:Z
+Volume=${GRAFANA_ETC_DIR}/grafana.ini:/etc/grafana/grafana.ini:Z
+Volume=/etc/localtime:/etc/localtime:ro
+Environment=TZ=Europe/Amsterdam
+
+[Install]
+${START_ON_BOOT}
+EOF
+}
+
+function configure() {
+  podman exec -ti grafana grafana-cli admin reset-admin-password "${ADMIN_PWD}"
+}
+
+function setEnvVars() {
+  setDefaultEnvVars
+  SERVICE_NAME="grafana"
+  GRAFANA_DATA_DIR="$(yq -r '.GRAFANA.DATA_DIR' "${CONFIG_YAML}")"
+  GRAFANA_ETC_DIR="$(yq -r '.GRAFANA.ETC_DIR' "${CONFIG_YAML}")"
+  SERVER_IP=$(hostname -I | awk '{print $1}')
+  GRAFANA_HTTP_PORT="$(yq -r '.GRAFANA.HTTP_PORT' "${CONFIG_YAML}")"
+  PODMAN_AUTO_UPDATE_STRATEGY="$(yq -r '.HOST.PODMAN_AUTO_UPDATE_STRATEGY' "${CONFIG_YAML}")"
+  CONTAINER_IMAGE="$(yq -r '.GRAFANA.CONTAINER_IMAGE' "${CONFIG_YAML}")" 
+  START_ON_BOOT="$(yq -r '.GRAFANA.START_ON_BOOT' "${CONFIG_YAML}")" 
+  ADMIN_PWD="$(yq -r '.GRAFANA.ADMIN_PWD' "${CONFIG_YAML}")"
+  DO_CONFIG="false"
+}
+
+
+function printEnvVars() {
+  echo CONFIG_YAML=${CONFIG_YAML}
+  echo QUADLET_DIR=${QUADLET_DIR}
+  echo SYSTEMD_UNIT_DIR=${SYSTEMD_UNIT_DIR}
+  echo SYSTEMCTL_CMD=${SYSTEMCTL_CMD}
+  echo NETWORK_NAME=${NETWORK_NAME}
+  echo PODMAN_AUTO_UPDATE_STRATEGY=${PODMAN_AUTO_UPDATE_STRATEGY}
+  echo GRAFANA_DATA_DIR=${GRAFANA_DATA_DIR}
+  echo GRAFANA_ETC_DIR=${GRAFANA_ETC_DIR}
+  echo GRAFANA_HTTP_PORT=${GRAFANA_HTTP_PORT}
+  echo CONTAINER_IMAGE=${CONTAINER_IMAGE}
+  echo START_ON_BOOT=${START_ON_BOOT}
+  echo ADMIN_PWD=${ADMIN_PWD}
+}
+
+function install() {
+  CreatePodmanNetwork
+  CreateQuadlet
+  postInstall
+  if [ ${DO_CONFIG} == true]; then
+    configure
+  fi  
+  showStatus
+}
+
+main "$@"
--- quadlet/prometheusQuadlet.sh
+++ quadlet/prometheusQuadlet.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
+source /links/bin/lib/quadletFunctions.sh
+
 function CreateQuadlet() {
   mkdir -p ${PROMETHEUS_DATA_DIR}
+  chmod 777 -R ${PROMETHEUS_DATA_DIR}
+  chown -R 65534:65534 ${PROMETHEUS_DATA_DIR}
+  chcon -t container_file_t ${PROMETHEUS_DATA_DIR}
   cat <<EOF > ${QUADLET_DIR}/${SERVICE_NAME}.container 
 [Unit]
 Description=${SERVICE_NAME}
@@ -16,55 +21,50 @@ ContainerName=${SERVICE_NAME}
 Image=${CONTAINER_IMAGE}
 Network=${NETWORK_NAME}
 PublishPort=${PROMETHEUS_HTTP_PORT}:9090
-Volume=${PROMETHEUS_DATA_DIR}:/prometheus,z
-Volume=${PROMETHEUS_ETC_DIR}:/etc/prometheus,z
-AddCapability=CAP_AUDIT_WRITE
+Volume=${PROMETHEUS_DATA_DIR}:/prometheus:Z
+Volume=${PROMETHEUS_ETC_DIR}/prometheus:/etc/prometheus:Z
+Volume=/etc/localtime:/etc/localtime:ro
+Environment=TZ=Europe/Amsterdam
 
 [Install]
-WantedBy=default.target
+${START_ON_BOOT}
 EOF
 }
 
 function CreatePrometheusConfig() {
+  # inspired by https://medium.com/@netopschic/implementing-monitoring-stack-node-exporter-prometheus-grafana-on-fedora-using-podman-compose-6bc97d4c44a9
   mkdir -p ${PROMETHEUS_ETC_DIR}/prometheus
+  # chown -R 65534:65534 ${PROMETHEUS_ETC_DIR}
+  # chcon -t container_file_t ${PROMETHEUS_ETC_DIR}
   cat <<EOF > ${PROMETHEUS_ETC_DIR}/prometheus/prometheus.yml
+# my global config
+global:
+  scrape_interval: 15s # Set the scrape interval to every 15 seconds. Default is every 1 minute.
+  evaluation_interval: 15s # Evaluate rules every 15 seconds. The default is every 1 minute.
+  # scrape_timeout is set to the global default (10s).
+
+scrape_configs:
+  # The job name is added as a label job=<job_name> to any timeseries scraped from this config.
+  - job_name: "prometheus"
+    # metrics_path defaults to '/metrics'
+    # scheme defaults to 'http'.
+    static_configs:
+      - targets: ["localhost:9090"]
+  - job_name: "node"
+    static_configs:
+      - targets: ["node-exporter:9100"]
 EOF
 }
 
-function CreatePodmanNetwork() {
-  podman network create ${NETWORK_NAME} --ignore
-}
-
-
-function postInstall() {
-  ${SYSTEMCTL_CMD} daemon-reload
-  ${SYSTEMCTL_CMD} start ${SERVICE_NAME}.service
-}
-
-function uninstall() {
-  ${SYSTEMCTL_CMD} stop ${SERVICE_NAME}.service
-  rm ${QUADLET_DIR}/${SERVICE_NAME}.container
-}
-
 function setEnvVars() {
+  setDefaultEnvVars
   SERVICE_NAME="prometheus"
-  if [[ $(id -u) -eq 0 ]] ; then 
-    echo "running as USER root" 
-    export QUADLET_DIR=/etc/containers/systemd
-    export SYSTEMD_UNIT_DIR=/etc/systemd/system
-    export SYSTEMCTL_CMD="systemctl"
-  else  
-    echo "running as USER ${USER}" 
-    export QUADLET_DIR=${HOME}/.config/containers/systemd
-    export SYSTEMD_UNIT_DIR=${HOME}/.config/systemd/user
-    export SYSTEMCTL_CMD="systemctl --user"
-  fi
-  NETWORK_NAME="$(yq -r '.HOST.PODMAN_NETWORK_NAME' "${CONFIG_YAML}")"
   PROMETHEUS_DATA_DIR="$(yq -r '.PROMETHEUS.DATA_DIR' "${CONFIG_YAML}")"
   PROMETHEUS_ETC_DIR="$(yq -r '.PROMETHEUS.ETC_DIR' "${CONFIG_YAML}")"
   SERVER_IP=$(hostname -I | awk '{print $1}')
   PROMETHEUS_HTTP_PORT="$(yq -r '.PROMETHEUS.HTTP_PORT' "${CONFIG_YAML}")"
   PODMAN_AUTO_UPDATE_STRATEGY="$(yq -r '.HOST.PODMAN_AUTO_UPDATE_STRATEGY' "${CONFIG_YAML}")"
+  START_ON_BOOT="$(yq -r '.PROMETHEUS.START_ON_BOOT' "${CONFIG_YAML}")" 
   CONTAINER_IMAGE="$(yq -r '.PROMETHEUS.CONTAINER_IMAGE' "${CONFIG_YAML}")" 
 }
 
@@ -92,87 +92,15 @@ function printEnvVars() {
   echo PROMETHEUS_ETC_DIR=${PROMETHEUS_ETC_DIR}
   echo PROMETHEUS_HTTP_PORT=${PROMETHEUS_HTTP_PORT}
   echo CONTAINER_IMAGE=${CONTAINER_IMAGE}
+  echo START_ON_BOOT=${START_ON_BOOT}
 }
 
 function install() {
   CreatePodmanNetwork
-  CreateQuadlet
   CreatePrometheusConfig
+  CreateQuadlet
   postInstall
   showStatus
 }
 
-
-function usage() {
-  echo "##################"
-  echo "Parameters available"
-  echo "-c <path-to-config-file> (required) "
-  echo "-i to install ${SERVICE_NAME}"
-  echo "-u to uninstall ${SERVICE_NAME}"
-  echo "-s to show status of ${SERVICE_NAME} "
-}
-
-function checkpCLIParams() {
-  while getopts "iusc:" OPTNAME; do
-    case "${OPTNAME}" in
-      i )
-        echo "Runmode Option ${OPTNAME} is specified"
-        RUN_MODE="INSTALL"
-        ;;
-      u )
-        echo "Runmode Option ${OPTNAME} is specified"
-        RUN_MODE="UNINSTALL"
-        ;;
-      s )
-        echo "Runmode Option ${OPTNAME} is specified"
-        RUN_MODE="STATUS"
-        ;;
-      c )
-        echo "config file used is \"${OPTARG}\" is specified"
-        CONFIG_YAML="${OPTARG}"
-        ;;
-      * )
-        echo "unknown parameter specified"
-        usage
-        exit 1
-        ;;
-    esac
-  done
-  if [ $OPTIND -eq 1 ]; then 
-    echo "No options were passed"; 
-    usage
-    exit 1
-  fi
-  if [ -z "${CONFIG_YAML+x}" ]  || [ ! -f "${CONFIG_YAML}" ]; then 
-    echo "Config file does not exist Please specify an existing config file witch -c"; 
-    usage
-  fi
-  if [ -z "${RUN_MODE+x}" ]; then
-     echo "No Runmode mode specified specify either -c or -u"
-     usage
-     exit 1
-  fi   
-}
-
-# main start here
-checkpCLIParams "$@"
-setEnvVars
-printEnvVars
-case "${RUN_MODE}" in 
-   "INSTALL" )
-     echo "installing"
-     install ;;
-   "UNINSTALL")
-     echo "uninstalling"
-     uninstall ;;
-   "STATUS")
-     echo "showing status"
-     showStatus ;;
-   * )
-     echo "Invalid Installation mode specified specifed us either -c or -u parameter"
-     usage; 
-     exit 1
-     ;;
- esac    
-
-
+main "$@"
